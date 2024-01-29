9321602fe37af7fba46d0ed94934f1b6ef4edb4f
Valentin Damlencourt <valentin_damlencourt@ens.univ-artois.fr>
2024-01-29 16:15:29 +0100

    Ajout HttpException message personnalisé

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use http\Env\Request;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +27,13 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+        $this->reportable(function (NotFoundHttpException $e, Request $request) {
+           if($request->is('api/*')) {
+               return response()->json([
+                   'message' => 'Enregistrement inconnu.'
+               ], 404);
+            }
         });
     }
 }
