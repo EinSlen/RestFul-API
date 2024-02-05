43bd76ca79fe4de2eec59d7e3927a8e84106b373
Valentin Damlencourt <valentin_damlencourt@ens.univ-artois.fr>
2024-02-05 16:27:09 +0100

    middleware role api.php

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('salles', [\App\Http\Controllers\Api\SalleController::class, 'index'])->name("salles.index");
 Route::get('salles/{id}', [\App\Http\Controllers\Api\SalleController::class, 'show'])->where("id","^[0-9]+$")->name("salles.s");
 
-Route::post('salles', [\App\Http\Controllers\Api\SalleController::class, 'store'])->name("salles.store");
+Route::post('salles', [\App\Http\Controllers\Api\SalleController::class, 'store'])
+    ->middleware('auth', 'role:'.\App\Models\Role::CREATE_SALLE)->name("salles.store");
 
 Route::put('salles/{id}', [\App\Http\Controllers\Api\SalleController::class, 'update'])->name("salles.update");
 Route::delete('salles/{id}', [\App\Http\Controllers\Api\SalleController::class, 'destroy'])->name("salles.delete");
