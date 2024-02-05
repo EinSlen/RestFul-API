67d237b3c8efdd67a7bd6450170841a596711bd1
Valentin Damlencourt <valentin_damlencourt@ens.univ-artois.fr>
2024-02-05 17:18:18 +0100

    modification api, controller

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('salles', [\App\Http\Controllers\Api\SalleController::class, 'index'])->name("salles.index");
-Route::get('salles/{id}', [\App\Http\Controllers\Api\SalleController::class, 'show'])->where("id","^[0-9]+$")->name("salles.s");
+Route::controller(\App\Http\Controllers\Api\AuthController::class)->group(function (){
+    Route::post('login', 'login')->name('login');
+    Route::post('register', 'register')->name('register');
+    Route::post('logout', 'logout')->name('logout');
+    Route::post('refresh', 'refresh')->name('refresh');
+
+});
+//gestion des jetons
+//modifier class model user
+//controller d'authentification (login, register, logout, refresh)
+//gérer les roles utilisateur (table role 5 roles)
+//relier les roles aux utilisateurs (table pivot)
+//mettre en place le controlle d'accès
+
+Route::get('salles', [\App\Http\Controllers\Api\SalleController::class, 'index'])->name("salles.index")
+    ->middleware('auth:api', 'role:'.\App\Models\Role::VISITEUR)->name("salles.show");
+Route::get('salles/{id}', [\App\Http\Controllers\Api\SalleController::class, 'show'])->where("id","^[0-9]+$")
+    ->middleware('auth:api', 'role:'.\App\Models\Role::VIEW_SALLE)->name("salles.show");
 
 Route::post('salles', [\App\Http\Controllers\Api\SalleController::class, 'store'])
-    ->middleware('auth', 'role:'.\App\Models\Role::CREATE_SALLE)->name("salles.store");
+    ->middleware('auth:api', 'role:'.\App\Models\Role::CREATE_SALLE)->name("salles.store");
 
-Route::put('salles/{id}', [\App\Http\Controllers\Api\SalleController::class, 'update'])->name("salles.update");
-Route::delete('salles/{id}', [\App\Http\Controllers\Api\SalleController::class, 'destroy'])->name("salles.delete");
+Route::put('salles/{id}', [\App\Http\Controllers\Api\SalleController::class, 'update'])
+    ->middleware('auth:api', 'role:'.\App\Models\Role::EDIT_SALLE)->name("salles.update");
+Route::delete('salles/{id}', [\App\Http\Controllers\Api\SalleController::class, 'destroy'])
+    ->middleware('auth:api', 'role:'.\App\Models\Role::ADMIN)->name("salles.delete");
 
 //Route::apiResource('salles', SalleController::class);
