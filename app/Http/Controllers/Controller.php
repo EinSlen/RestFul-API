c83f74d124446bddf085aac54cd1dd06bebad645
Fred Hémery <fred.hemery@univ-artois.fr>
2024-02-05 17:32:06 +0100

    :construction: Ajoute la documentation (en cours)

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Attributes as OA;
 
-class Controller extends BaseController
-{
+#[OA\Info(version: "1.0.0",
+    description: "Serveur Api Documentation pour l'application activité-sport",
+    title: "Serveur Api Activité Sport",
+)]
+#[OA\Server(description:"Docs serveur", url: "http://localhost:8000/api")]
+#[OA\SecurityScheme(securityScheme: 'bearerAuth', type: 'http', bearerFormat: 'JWT', scheme: 'bearer')]
+class OpenApi {}
+
+class Controller extends BaseController {
     use AuthorizesRequests, ValidatesRequests;
 }
