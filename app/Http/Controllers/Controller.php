fcb6d5ceb282e1edaeb584254e824d778cae86e9
Valentin Damlencourt <valentin_damlencourt@ens.univ-artois.fr>
2024-02-05 17:34:41 +0100

    open api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(version: "1.0.0", description: "sERVEUR api documentation application activité sport",
+    title: "API activité sport")]
+#[OA\Server(description:"serveur api", url: "http://localhost:8000/api/salles")]
+#[OA\SecurityScheme()]
+class OpenApi {}
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
